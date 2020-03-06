a94468c4ea36d87a3ca86e29a6a63bd67849488d
Romáio Arruda <35845386+romarioarruda@users.noreply.github.com>
2020-03-06 18:29:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,21 +8,18 @@ $formatoAtual = $argv[1];
 
 $formatoNovo = $argv[2];
 
+$prefixo = "";
+
 if(isset($argv[3])){
 	$prefixo = $argv[3];
-}
-else{
-	$prefixo = "";
 }
 
 $arquivos = glob("{*.png,*.jpg,*.jpeg,*.gif,*.pdf}", GLOB_BRACE);
 foreach($arquivos as $img){
 	$nome = str_replace(".{$formatoAtual}", "", $img);
-	// $convert = "convert -quality 100 -density 300 ".$img." convertidas/".$prefixo.$nome.".{$formatoNovo}";
-	 $convert = "convert ".$img." ".$prefixo.$nome.".{$formatoNovo}";
-	// $convert='pngtopnm  -background "#FFFFFF" -mix  "'.$img.'" | pnmtojpeg > "convertidas/'.$prefixo.$nome.'.'.$formatoNovo.'"';
-	`$convert`;
-	echo $img." => ".$prefixo.$nome.".".$formatoNovo."\n";
+	
+	exec("convert $img $prefixo.$nome.".".{$formatoNovo}");
+
+	echo $img." => ". $prefixo.$nome.".".$formatoNovo \n";
 }
 
-?>
